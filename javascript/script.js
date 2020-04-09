be67dc66287fdf37c94ae8a929471ccdd8d5d2f6
Phillip Watson <phillipawatson96@gmail.com>
2020-04-09 19:58:53 -0400

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+function changeTextBy(){
+  document.getElementById('black').innerHTML = "BY ";
+  document.getElementById('hide').innerHTML = '';
+};
+
+function changeTextWhy(){
+  document.getElementById('black').innerHTML = "WHY ";
+  document.getElementById('hide').innerHTML = '';
+};
+
+function changeTextFind(){
+  document.getElementById('black').innerHTML = "FIND ";
+  document.getElementById('hide').innerHTML = '';
+};
+
+function changeBack(){
+  document.getElementById('black').innerHTML = "";
+  document.getElementById('hide').innerHTML = 'DESIGN';
+}
